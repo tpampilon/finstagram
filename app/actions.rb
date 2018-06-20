b799adafa56eeece0c5590741f2fe9e8070bf051
Ted Pampilon <ted.pampilon@gmail.com>
2018-06-20 01:55:34 +0000

    login class

--- app/actions.rb
+++ app/actions.rb
@@ -1,16 +1,57 @@
+
+helpers do
+  def current_user
+      User.find_by(id: session[:user_id])
+  end
+end
+            
 get '/' do
  
   @finstagram_posts = FinstagramPost.order(created_at: :desc)
-  erb (:index)
+  erb(:index)
   
 end
+
+
+get '/login' do
+
+  erb(:login)
+
+end
+
+
+post '/login' do
   
+  username = params[:username]
+  password = params[:password]
+
+ @user = User.find_by(username: username)  
+
+ if @user && @user.password == password
+   session[:user_id] = @user.id
+   redirect to('/')
+ else
+   @error_message = "Login failed."
+   erb(:login)
+ end
+
+end
+
+get '/logout' do
+  
+  session[:user_id] = nil
+  redirect to('/')
+
+end
+
+
 get '/signup' do
     
     @user = User.new
     erb(:signup)
 
 end
+
 
 post '/signup' do
   
@@ -22,7 +63,7 @@ post '/signup' do
   @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password })
 
   if @user.save
-    "User #{username} saved!"
+    redirect to('/login')
   else
     erb(:signup)
   end
